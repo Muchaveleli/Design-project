e840b62b1616dbc65336f13db31dff987eb473c3
Muchaveleli Manjate <1653288@students.wits.ac.za>
2022-09-09 01:01:10 +0200

    images

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Data from '../components/data'
+import * as styles from '../styles/results.module.css'
 
 export default function results() {
   return (
-    <Layout>
+    <Layout classname={styles.all}>
         <h3>Here are your energy log results</h3>
         <iframe src="https://www.wikiloc.com/wikiloc/spatialArtifacts.do?event=view&id=113080396&measures=off&title=off&near=off&images=off&maptype=H"
         title='results'
